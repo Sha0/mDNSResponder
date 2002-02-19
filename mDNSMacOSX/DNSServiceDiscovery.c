41d69640d5355c7074fca94d159c1ea6c060fce5
epeyton <epeyton@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2002-02-19 20:36:19 +0000

    Merged in branches PR-2609050 PR-2618481 PR-2857501 PR-2862847
    Bug #:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@14 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSMacOSX/DNSServiceDiscovery.c
@@ -0,0 +1,481 @@
+/*
+ * Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+                                                               * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ *
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+#include "DNSServiceDiscovery.h"
+#include "DNSServiceDiscoveryDefines.h"
+
+#include "DNSServiceDiscoveryRequest.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <servers/bootstrap.h>
+#include <mach/mach_init.h>
+#include <mach/mach_error.h>
+#include <mach/mach_types.h>
+#include <pthread.h>
+
+#include <netinet/in.h>
+
+extern struct rpc_subsystem internal_DNSServiceDiscoveryReply_subsystem;
+
+extern boolean_t DNSServiceDiscoveryReply_server(
+        mach_msg_header_t *InHeadP,
+        mach_msg_header_t *OutHeadP);
+
+struct a_requests {
+    struct a_requests		*next;
+    mach_port_t				client_port;
+    union {
+        DNSServiceBrowserReply 				browserCallback;
+        DNSServiceDomainEnumerationReply 	enumCallback;
+        DNSServiceRegistrationReply 		regCallback;
+        DNSServiceResolverReply 			resolveCallback;
+    } callout;
+    void					*context;
+};
+
+static struct a_requests	*a_requests	= NULL;
+static pthread_mutex_t		a_requests_lock	= PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct _dns_service_discovery_t {
+    mach_port_t	port;
+} dns_service_discovery_t;
+
+mach_port_t DNSServiceDiscoveryLookupServer(void)
+{
+    static mach_port_t sndPort 	= MACH_PORT_NULL;
+    kern_return_t   result;
+
+    if (sndPort != MACH_PORT_NULL) {
+        return sndPort;
+    }
+
+    result = bootstrap_look_up(bootstrap_port, DNS_SERVICE_DISCOVERY_SERVER, &sndPort);
+    if (result != KERN_SUCCESS) {
+        printf("%s(): {%s:%d} bootstrap_look_up() failed: $%x\n", __FUNCTION__, __FILE__, __LINE__, (int) result);
+        sndPort = MACH_PORT_NULL;
+    }
+
+    return sndPort;
+}
+
+dns_service_discovery_ref DNSServiceBrowserCreate (char *regtype, char *domain, DNSServiceBrowserReply callBack,void *context)
+{
+    mach_port_t serverPort = DNSServiceDiscoveryLookupServer();
+    mach_port_t clientPort;
+    kern_return_t result;
+    dns_service_discovery_ref return_t;
+    struct a_requests	*request;
+    
+    if (!serverPort) {
+        return NULL;
+    }
+
+    result = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &clientPort);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port receive creation failed, %s\n", mach_error_string(result));
+        return NULL;
+    }
+    result = mach_port_insert_right(mach_task_self(), clientPort, clientPort, MACH_MSG_TYPE_MAKE_SEND);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port send creation failed, %s\n", mach_error_string(result));
+        mach_port_destroy(mach_task_self(), clientPort);
+        return NULL;
+    }
+
+    return_t = malloc(sizeof(dns_service_discovery_t));
+    return_t->port = clientPort;
+
+    request = malloc(sizeof(struct a_requests));
+    request->client_port = clientPort;
+    request->context = context;
+    request->callout.browserCallback = callBack;
+
+    result = DNSServiceBrowserCreate_rpc(serverPort, clientPort, regtype, domain);
+
+    if (result != KERN_SUCCESS) {
+        printf("There was an error creating a browser, %s\n", mach_error_string(result));
+        free(request);
+        return NULL;
+    }
+
+    pthread_mutex_lock(&a_requests_lock);
+    request->next = a_requests;
+    a_requests = request;
+    pthread_mutex_unlock(&a_requests_lock);
+    
+    return return_t;
+}
+
+/* Service Enumeration */
+
+dns_service_discovery_ref DNSServiceDomainEnumerationCreate (int registrationDomains, DNSServiceDomainEnumerationReply callBack, void *context)
+{
+    mach_port_t serverPort = DNSServiceDiscoveryLookupServer();
+    mach_port_t clientPort;
+    kern_return_t result;
+    dns_service_discovery_ref return_t;
+    struct a_requests	*request;
+
+    if (!serverPort) {
+        return NULL;
+    }
+
+    result = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &clientPort);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port receive creation failed, %s\n", mach_error_string(result));
+        return NULL;
+    }
+    result = mach_port_insert_right(mach_task_self(), clientPort, clientPort, MACH_MSG_TYPE_MAKE_SEND);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port send creation failed, %s\n", mach_error_string(result));
+        mach_port_destroy(mach_task_self(), clientPort);
+        return NULL;
+    }
+    return_t = malloc(sizeof(dns_service_discovery_t));
+    return_t->port = clientPort;
+
+    request = malloc(sizeof(struct a_requests));
+    request->client_port = clientPort;
+    request->context = context;
+    request->callout.enumCallback = callBack;
+
+    result = DNSServiceDomainEnumerationCreate_rpc(serverPort, clientPort, registrationDomains);
+
+    if (result != KERN_SUCCESS) {
+        printf("There was an error creating an enumerator, %s\n", mach_error_string(result));
+        free(request);
+        return NULL;
+    }
+    
+    pthread_mutex_lock(&a_requests_lock);
+    request->next = a_requests;
+    a_requests = request;
+    pthread_mutex_unlock(&a_requests_lock);
+
+    return return_t;
+}
+
+
+/* Service Registration */
+
+dns_service_discovery_ref DNSServiceRegistrationCreate
+(char *name, char *regtype, char *domain, Opaque16 port, char *txtRecord, DNSServiceRegistrationReply callBack, void *context)
+{
+    mach_port_t serverPort = DNSServiceDiscoveryLookupServer();
+    mach_port_t clientPort;
+    kern_return_t		result;
+    dns_service_discovery_ref return_t;
+    struct a_requests	*request;
+    
+    if (!serverPort) {
+        return NULL;
+    }
+
+    result = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &clientPort);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port receive creation failed, %s\n", mach_error_string(result));
+        return NULL;
+    }
+    result = mach_port_insert_right(mach_task_self(), clientPort, clientPort, MACH_MSG_TYPE_MAKE_SEND);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port send creation failed, %s\n", mach_error_string(result));
+        mach_port_destroy(mach_task_self(), clientPort);
+        return NULL;
+    }
+    return_t = malloc(sizeof(dns_service_discovery_t));
+    return_t->port = clientPort;
+
+    request = malloc(sizeof(struct a_requests));
+    request->client_port = clientPort;
+    request->context = context;
+    request->callout.regCallback = callBack;
+
+    result = DNSServiceRegistrationCreate_rpc(serverPort, clientPort, name, regtype, domain, (int)(port.NotAnInteger), txtRecord);
+
+    if (result != KERN_SUCCESS) {
+        printf("There was an error creating a resolve, %s\n", mach_error_string(result));
+        free(request);
+        return NULL;
+    }
+
+    pthread_mutex_lock(&a_requests_lock);
+    request->next = a_requests;
+    a_requests = request;
+    pthread_mutex_unlock(&a_requests_lock);
+
+    return return_t;
+}
+
+/* Resolver requests */
+
+dns_service_discovery_ref DNSServiceResolverResolve(char *name, char *regtype, char *domain, DNSServiceResolverReply callBack, void *context)
+{
+    mach_port_t serverPort = DNSServiceDiscoveryLookupServer();
+    mach_port_t clientPort;
+    kern_return_t		result;
+    dns_service_discovery_ref return_t;
+    struct a_requests	*request;
+
+    if (!serverPort) {
+        return NULL;
+    }
+
+    result = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &clientPort);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port receive creation failed, %s\n", mach_error_string(result));
+        return NULL;
+    }
+    result = mach_port_insert_right(mach_task_self(), clientPort, clientPort, MACH_MSG_TYPE_MAKE_SEND);
+    if (result != KERN_SUCCESS) {
+        printf("Mach port send creation failed, %s\n", mach_error_string(result));
+        mach_port_destroy(mach_task_self(), clientPort);
+        return NULL;
+    }
+    return_t = malloc(sizeof(dns_service_discovery_t));
+    return_t->port = clientPort;
+
+    request = malloc(sizeof(struct a_requests));
+    request->client_port = clientPort;
+    request->context = context;
+    request->callout.resolveCallback = callBack;
+
+    DNSServiceResolverResolve_rpc(serverPort, clientPort, name, regtype, domain);
+
+    pthread_mutex_lock(&a_requests_lock);
+    request->next = a_requests;
+    a_requests = request;
+    pthread_mutex_unlock(&a_requests_lock);
+
+    return return_t;
+}
+
+void DNSServiceDiscovery_handleReply(void *replyMsg)
+{
+    unsigned long			result = 0xFFFFFFFF;
+    mach_msg_header_t *    	msgSendBufPtr;
+    mach_msg_header_t *     receivedMessage;
+    unsigned        		msgSendBufLength;
+
+    msgSendBufLength = internal_DNSServiceDiscoveryReply_subsystem.maxsize;
+    msgSendBufPtr = (mach_msg_header_t *) malloc(msgSendBufLength);
+
+
+    receivedMessage = ( mach_msg_header_t * ) replyMsg;
+
+    // Call DNSServiceDiscoveryReply_server to change mig-generated message into a
+    // genuine mach message. It will then cause the callback to get called.
+    result = DNSServiceDiscoveryReply_server ( receivedMessage, msgSendBufPtr );
+    ( void ) mach_msg_send ( msgSendBufPtr );
+    free(msgSendBufPtr);
+}
+
+mach_port_t DNSServiceDiscoveryMachPort(dns_service_discovery_ref dnsServiceDiscovery)
+{
+    return dnsServiceDiscovery->port;
+}
+
+void DNSServiceDiscoveryDeallocate(dns_service_discovery_ref dnsServiceDiscovery)
+{
+    struct a_requests	*request0, *request;
+    mach_port_t reply = dnsServiceDiscovery->port;
+
+    if (dnsServiceDiscovery->port) {
+        pthread_mutex_lock(&a_requests_lock);
+        request0 = NULL;
+        request  = a_requests;
+        while (request) {
+               if (request->client_port == reply) {
+                /* request info found, remove from list */
+                if (request0) {
+                    request0->next = request->next;
+                } else {
+                    a_requests = request->next;
+                }
+                break;
+            }
+        }
+        pthread_mutex_unlock(&a_requests_lock);
+
+        free(request);
+        
+        mach_port_destroy(mach_task_self(), dnsServiceDiscovery->port);
+    }
+    return;
+}
+
+// reply functions, calls the users setup callbacks with function pointers
+
+kern_return_t internal_DNSServiceDomainEnumerationReply_rpc
+(
+    mach_port_t reply,
+    int resultType,
+    DNSCString replyDomain,
+    int flags
+)
+{
+    struct a_requests	*request;
+    void *requestContext = NULL;
+    DNSServiceDomainEnumerationReply callback = NULL;
+
+    pthread_mutex_lock(&a_requests_lock);
+    request  = a_requests;
+    while (request) {
+        if (request->client_port == reply) {
+            break;
+        }
+    }
+
+    if (request != NULL) {
+        callback = (*request->callout.enumCallback);
+        requestContext = request->context;
+    }
+    pthread_mutex_unlock(&a_requests_lock);
+
+    if (request != NULL) {
+        (callback)(resultType, replyDomain, flags, requestContext);
+    }
+    
+    return KERN_SUCCESS;
+
+}
+
+kern_return_t internal_DNSServiceBrowserReply_rpc
+(
+    mach_port_t reply,
+    int resultType,
+    DNSCString replyName,
+    DNSCString replyType,
+    DNSCString replyDomain,
+    int flags
+)
+{
+    struct a_requests	*request;
+    void *requestContext = NULL;
+    DNSServiceBrowserReply callback = NULL;
+
+    pthread_mutex_lock(&a_requests_lock);
+    request  = a_requests;
+    while (request) {
+        if (request->client_port == reply) {
+            break;
+        }
+    }
+    if (request != NULL) {
+        callback = (*request->callout.browserCallback);
+        requestContext = request->context;
+    }
+
+    pthread_mutex_unlock(&a_requests_lock);
+
+    if (request != NULL) {
+        (callback)(resultType, replyName, replyType, replyDomain, flags, requestContext);
+    }
+
+    return KERN_SUCCESS;
+}
+
+
+kern_return_t internal_DNSServiceRegistrationReply_rpc
+(
+    mach_port_t reply,
+    int resultType
+)
+{
+    struct a_requests	*request;
+    void *requestContext = NULL;
+    DNSServiceRegistrationReply callback = NULL;
+
+    pthread_mutex_lock(&a_requests_lock);
+    request  = a_requests;
+    while (request) {
+        if (request->client_port == reply) {
+            break;
+        }
+    }
+    if (request != NULL) {
+        callback = (*request->callout.regCallback);
+        requestContext = request->context;
+    }
+
+    pthread_mutex_unlock(&a_requests_lock);
+    if (request != NULL) {
+        (callback)(resultType, requestContext);
+    }
+    return KERN_SUCCESS;
+}
+
+
+kern_return_t internal_DNSServiceResolverReply_rpc
+(
+    mach_port_t reply,
+    sockaddr_t interface,
+    sockaddr_t address,
+    DNSCString txtRecord,
+    int flags
+)
+{
+    struct sockaddr  *interface_storage = NULL;
+    struct sockaddr  *address_storage = NULL;
+    struct a_requests	*request;
+    void *requestContext = NULL;
+    DNSServiceResolverReply callback = NULL;
+
+    if (interface) {
+        int len = ((struct sockaddr *)interface)->sa_len;
+        interface_storage = (struct sockaddr *)malloc(len);
+        bcopy(interface, interface_storage,len);
+    }
+
+    if (address) {
+        int len = ((struct sockaddr *)address)->sa_len;
+        address_storage = (struct sockaddr *)malloc(len);
+        bcopy(address, address_storage, len);
+    }
+
+    pthread_mutex_lock(&a_requests_lock);
+    request  = a_requests;
+    while (request) {
+        if (request->client_port == reply) {
+            break;
+        }
+    }
+
+    if (request != NULL) {
+        callback = (*request->callout.resolveCallback);
+        requestContext = request->context;
+    }
+    pthread_mutex_unlock(&a_requests_lock);
+
+    if (request != NULL) {
+        (callback)(interface_storage, address_storage, txtRecord, flags, requestContext);
+    }
+
+    if (interface) {
+        free(interface_storage);
+    }
+    if (address) {
+        free(address_storage);
+    }
+    
+    return KERN_SUCCESS;
+}
---
+++ mDNSMacOSX/DNSServiceDiscovery.h
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+* "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ *
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+#ifndef __DNS_SERVICE_DISCOVERY_H
+#define __DNS_SERVICE_DISCOVERY_H
+
+#include <mach/mach_types.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+typedef union { unsigned char b[2]; u_int16_t NotAnInteger; } Opaque16;
+
+/* Opaque internal data type */
+typedef struct _dns_service_discovery_t * dns_service_discovery_ref;
+
+/* possible reply flags values */
+
+enum {
+    kDNSServiceDiscoveryNoFlags			= 0,
+    kDNSServiceDiscoveryMoreRepliesImmediately	= 1 << 0,
+};
+
+/*!
+@function DNSServiceResolver_handleReply
+ @description This function should be called with the Mach message sent
+ to the port returned by the call to DNSServiceResolverResolve.
+ The reply message will be interpreted and will result in a
+ call to the specified callout function.
+ @param replyMsg The Mach message.
+ */
+void DNSServiceDiscovery_handleReply(void *replyMsg);
+
+/* Service Registration */
+
+typedef void (*DNSServiceRegistrationReply) (
+    int 		errorCode,		// -1 name collision, 0 successful registration, other uDNS error code 
+    void		*context
+);
+
+/*!
+@function DNSServiceRegistrationCreate
+    @description Register a named service with DNS Service Discovery
+    @param name The name of this service instance (e.g. "Steve's Printer")
+    @param regtype The service type (e.g. "_printer._tcp." -- see
+        RFC 2782 (DNS SRV) and <http://www.iana.org/assignments/port-numbers>)
+    @param domain The domain in which to register the service (e.g. "apple.com.")
+    @param port The local port on which this service is being offered
+    @param txtRecord Optional protocol-specific additional information
+    @param callBack The DNSServiceRegistrationReply function to be called
+    @param context A user specified context which will be passed to the callout function.
+    @result A dns_registration_t
+*/
+dns_service_discovery_ref DNSServiceRegistrationCreate
+(
+    char 		*name,
+    char 		*regtype,
+    char 		*domain,
+    Opaque16		port,
+    char 		*txtRecord,
+    DNSServiceRegistrationReply callBack,
+    void		*context
+);
+
+/***************************************************************************/
+/*   DNS Domain Enumeration   */
+
+typedef enum
+{
+    DNSServiceDomainEnumerationReplyAddDomain,			// Domain found
+    DNSServiceDomainEnumerationReplyAddDomainDefault,		// Domain found (and should be selected by default)
+    DNSServiceDomainEnumerationReplyRemoveDomain,			// Domain has been removed from network
+} DNSServiceDomainEnumerationReplyResultType;
+
+typedef void (*DNSServiceDomainEnumerationReply) (
+    int 		resultType,		// One of DNSServiceDomainEnumerationReplyResultType
+    char  		*replyDomain,
+    int 		flags,			// DNS Service Discovery reply flags information
+    void		*context		
+);
+
+/*!
+    @function DNSServiceDomainEnumerationCreate
+    @description Asynchronously create a DNS Domain Enumerator
+    @param registrationDomains A boolean indicating whether you are looking
+        for recommended registration domains
+        (e.g. equivalent to the AppleTalk zone list in the AppleTalk Control Panel)
+        or recommended browsing domains
+        (e.g. equivalent to the AppleTalk zone list in the Chooser).
+    @param callBack The function to be called when domains are found or removed
+    @param context A user specified context which will be passed to the callout function.
+    @result A dns_registration_t
+*/
+dns_service_discovery_ref DNSServiceDomainEnumerationCreate
+(
+    int 		registrationDomains,
+    DNSServiceDomainEnumerationReply	callBack,
+    void		*context
+);
+
+/***************************************************************************/
+/*   DNS Service Browser   */
+
+typedef enum
+{
+    DNSServiceBrowserReplyAddInstance,	// Instance of service found
+    DNSServiceBrowserReplyRemoveInstance	// Instance has been removed from network
+} DNSServiceBrowserReplyResultType;
+
+typedef void (*DNSServiceBrowserReply) (
+    int 		resultType,		// One of DNSServiceBrowserReplyResultType
+    char  		*replyName,
+    char  		*replyType,
+    char  		*replyDomain,
+    int 		flags,			// DNS Service Discovery reply flags information
+    void		*context
+);
+
+/*!
+    @function DNSServiceBrowserCreate
+    @description Asynchronously create a DNS Service browser
+    @param regtype The type of service
+    @param domain The domain in which to find the service
+    @param callBack The function to be called when service instances are found or removed
+    @param context A user specified context which will be passed to the callout function.
+    @result A dns_registration_t
+*/
+dns_service_discovery_ref DNSServiceBrowserCreate
+(
+    char 		*regtype,
+    char 		*domain,
+    DNSServiceBrowserReply	callBack,
+    void		*context
+);
+
+/***************************************************************************/
+/* Resolver requests */
+
+typedef void (*DNSServiceResolverReply) (
+    struct sockaddr 	*interface,		// Needed for scoped addresses like link-local
+    struct sockaddr 	*address,
+    char 		*txtRecord,
+    int 		flags,			// DNS Service Discovery reply flags information
+    void		*context
+);
+
+/*!
+@function DNSServiceResolverResolve
+    @description Resolved a named instance of a service to its address, port, and
+        (optionally) other demultiplexing information contained in the TXT record.
+    @param name The name of the service instance
+    @param regtype The type of service
+    @param domain The domain in which to find the service
+    @param callBack The DNSServiceResolverReply function to be called when the specified
+        address has been resolved.
+    @param context A user specified context which will be passed to the callout function.
+    @result A dns_registration_t
+*/
+
+dns_service_discovery_ref DNSServiceResolverResolve
+(
+    char 		*name,
+    char 		*regtype,
+    char 		*domain,
+    DNSServiceResolverReply callBack,
+    void		*context
+);
+
+/***************************************************************************/
+/* Mach port accessor and deallocation */
+
+/*!
+    @function DNSServiceDiscoveryMachPort
+    @description Returns the mach port for a dns_service_discovery_ref
+    @param registration A dns_service_discovery_ref as returned from DNSServiceRegistrationCreate
+    @result A mach reply port which will be sent messages as appropriate.
+        These messages should be passed to the DNSServiceDiscovery_handleReply
+        function.  A NULL value indicates that no address was
+        specified or some other error occurred which prevented the
+        resolution from being started.
+*/
+mach_port_t DNSServiceDiscoveryMachPort(dns_service_discovery_ref dnsServiceDiscovery);
+
+/*!
+    @function DNSServiceDiscoveryDeallocate
+    @description Deallocates the DNS Service Discovery type / closes the connection to the server
+    @param dnsServiceDiscovery A dns_service_discovery_ref as returned from a creation or enumeration call
+    @result void
+*/
+void DNSServiceDiscoveryDeallocate(dns_service_discovery_ref dnsServiceDiscovery);
+
+#endif	/* __DNS_SERVICE_DISCOVERY_H */
