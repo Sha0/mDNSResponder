f67c35483b8c64bef244072fadb78c74e674037b
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2002-03-30 01:10:37 +0000

    Initial checkin of external (portable) common source files
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@75 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSCore/mDNSsprintf.h
@@ -0,0 +1,1 @@
+#ifdef	__cplusplus	extern "C" {#endifextern int mDNS_sprintf(char *sbuffer, const char *fmt, ...);#ifdef	__cplusplus	}#endif
---
+++ mDNSCore/mDNSvsprintf.h
@@ -0,0 +1,1 @@
+#ifdef	__cplusplus	extern "C" {#endifextern int mDNS_vsprintf(char *sbuffer, const char *fmt, va_list arg);#ifdef	__cplusplus	}#endif
