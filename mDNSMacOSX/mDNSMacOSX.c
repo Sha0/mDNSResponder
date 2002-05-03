54a7730580145497d0452cf4c1761df68c8285fa
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2002-05-03 01:23:35 +0000

    Code contributed by Dave Heller: Define RUN_ON_PUMA_WITHOUT_IFADDRS to compile code that will work on Mac OS X 10.1, which does not have the getifaddrs call.
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@158 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSMacOSX/mDNSMacOSX.c
+++ mDNSMacOSX/mDNSMacOSX.c
@@ -37,7 +37,35 @@
 #include <sys/uio.h>
 #include <sys/param.h>
 #include <sys/socket.h>
+
+// Code contributed by Dave Heller:
+// Define RUN_ON_PUMA_WITHOUT_IFADDRS to compile code that will
+// work on Mac OS X 10.1, which does not have the getifaddrs call.
+#define RUN_ON_PUMA_WITHOUT_IFADDRS 0
+
+#if RUN_ON_PUMA_WITHOUT_IFADDRS
+#include <sys/ioctl.h>
+#include <sys/sockio.h>
+
+struct ifaddrs {
+	struct ifaddrs  *ifa_next;
+	char		*ifa_name;
+	u_int		 ifa_flags;
+	struct sockaddr	*ifa_addr;
+	struct sockaddr	*ifa_netmask;
+	struct sockaddr	*ifa_dstaddr;
+	void		*ifa_data;
+};
+#ifndef	ifa_broadaddr
+#define	ifa_broadaddr	ifa_dstaddr	/* broadcast address interface */
+#endif
+#include <sys/cdefs.h>
+
+#else
+
 #include <ifaddrs.h>
+
+#endif
 
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOMessage.h>
@@ -287,6 +315,227 @@ mDNSlocal NetworkInterfaceInfo2 *SearchForInterfaceByName(mDNS *const m, char *i
 	return(NULL);
 	}
 
+#if RUN_ON_PUMA_WITHOUT_IFADDRS
+
+/* Our own header for the programs that need interface configuration info.
+   Include this file, instead of "unp.h". */
+
+#define	IFA_NAME	16			/* same as IFNAMSIZ in <net/if.h> */
+#define	IFA_HADDR	 8			/* allow for 64-bit EUI-64 in future */
+
+struct ifa_info {
+  char    ifa_name[IFA_NAME];	/* interface name, null terminated */
+  u_char  ifa_haddr[IFA_HADDR];	/* hardware address */
+  u_short ifa_hlen;				/* #bytes in hardware address: 0, 6, 8 */
+  short   ifa_flags;			/* IFF_xxx constants from <net/if.h> */
+  short   ifa_myflags;			/* our own IFI_xxx flags */
+  struct sockaddr  *ifa_addr;	/* primary address */
+  struct sockaddr  *ifa_brdaddr;/* broadcast address */
+  struct sockaddr  *ifa_dstaddr;/* destination address */
+  struct ifa_info  *ifa_next;	/* next of these structures */
+};
+
+#define	IFI_ALIAS	1			/* ifa_addr is an alias */
+
+					/* function prototypes */
+struct ifa_info	*get_ifa_info(int, int);
+struct ifa_info	*Get_ifa_info(int, int);
+void			 free_ifa_info(struct ifa_info *);
+
+#define HAVE_SOCKADDR_SA_LEN	1
+
+struct ifa_info *
+get_ifa_info(int family, int doaliases)
+{
+	struct ifa_info		*ifi, *ifihead, **ifipnext;
+	int					sockfd, len, lastlen, flags, myflags;
+	char				*ptr, *buf, lastname[IFNAMSIZ], *cptr;
+	struct ifconf		ifc;
+	struct ifreq		*ifr, ifrcopy;
+	struct sockaddr_in	*sinptr;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	lastlen = 0;
+	len = 100 * sizeof(struct ifreq);	/* initial buffer size guess */
+	for ( ; ; ) {
+		buf = (char *) malloc(len);
+		ifc.ifc_len = len;
+		ifc.ifc_buf = buf;
+		if (ioctl(sockfd, SIOCGIFCONF, &ifc) < 0) {
+			if (errno != EINVAL || lastlen != 0)
+				debugf("ioctl error");
+		} else {
+			if (ifc.ifc_len == lastlen)
+				break;		/* success, len has not changed */
+			lastlen = ifc.ifc_len;
+		}
+		len += 10 * sizeof(struct ifreq);	/* increment */
+		free(buf);
+	}
+	ifihead = NULL;
+	ifipnext = &ifihead;
+	lastname[0] = 0;
+/* end get_ifa_info1 */
+
+/* include get_ifa_info2 */
+	for (ptr = buf; ptr < buf + ifc.ifc_len; ) {
+		ifr = (struct ifreq *) ptr;
+
+#ifdef	HAVE_SOCKADDR_SA_LEN
+		len = MAX(sizeof(struct sockaddr), ifr->ifr_addr.sa_len);
+#else
+		switch (ifr->ifr_addr.sa_family) {
+#ifdef	IPV6
+		case AF_INET6:	
+			len = sizeof(struct sockaddr_in6);
+			break;
+#endif
+		case AF_INET:	
+		default:	
+			len = sizeof(struct sockaddr);
+			break;
+		}
+#endif	/* HAVE_SOCKADDR_SA_LEN */
+		ptr += sizeof(ifr->ifr_name) + len;	/* for next one in buffer */
+
+		if (ifr->ifr_addr.sa_family != family)
+			continue;	/* ignore if not desired address family */
+
+		myflags = 0;
+		if ( (cptr = strchr(ifr->ifr_name, ':')) != NULL)
+			*cptr = 0;		/* replace colon will null */
+		if (strncmp(lastname, ifr->ifr_name, IFNAMSIZ) == 0) {
+			if (doaliases == 0)
+				continue;	/* already processed this interface */
+			myflags = IFI_ALIAS;
+		}
+		memcpy(lastname, ifr->ifr_name, IFNAMSIZ);
+
+		ifrcopy = *ifr;
+		ioctl(sockfd, SIOCGIFFLAGS, &ifrcopy);
+		flags = ifrcopy.ifr_flags;
+		if ((flags & IFF_UP) == 0)
+			continue;	/* ignore if interface not up */
+
+		ifi = (struct ifa_info *) calloc(1, sizeof(struct ifa_info));
+		*ifipnext = ifi;			/* prev points to this new one */
+		ifipnext = &ifi->ifa_next;	/* pointer to next one goes here */
+
+		ifi->ifa_flags = flags;		/* IFF_xxx values */
+		ifi->ifa_myflags = myflags;	/* IFI_xxx values */
+		memcpy(ifi->ifa_name, ifr->ifr_name, IFA_NAME);
+		ifi->ifa_name[IFA_NAME-1] = '\0';
+/* end get_ifa_info2 */
+/* include get_ifa_info3 */
+		switch (ifr->ifr_addr.sa_family) {
+		case AF_INET:
+			sinptr = (struct sockaddr_in *) &ifr->ifr_addr;
+			if (ifi->ifa_addr == NULL) {
+				ifi->ifa_addr = (struct sockaddr *) calloc(1, sizeof(struct sockaddr_in));
+				memcpy(ifi->ifa_addr, sinptr, sizeof(struct sockaddr_in));
+
+#ifdef	SIOCGIFBRDADDR
+				if (flags & IFF_BROADCAST) {
+					ioctl(sockfd, SIOCGIFBRDADDR, &ifrcopy);
+					sinptr = (struct sockaddr_in *) &ifrcopy.ifr_broadaddr;
+					ifi->ifa_brdaddr = (struct sockaddr *) calloc(1, sizeof(struct sockaddr_in));
+					memcpy(ifi->ifa_brdaddr, sinptr, sizeof(struct sockaddr_in));
+				}
+#endif
+
+#ifdef	SIOCGIFDSTADDR
+				if (flags & IFF_POINTOPOINT) {
+					ioctl(sockfd, SIOCGIFDSTADDR, &ifrcopy);
+					sinptr = (struct sockaddr_in *) &ifrcopy.ifr_dstaddr;
+					ifi->ifa_dstaddr = (struct sockaddr *) calloc(1, sizeof(struct sockaddr_in));
+					memcpy(ifi->ifa_dstaddr, sinptr, sizeof(struct sockaddr_in));
+				}
+#endif
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
+	free(buf);
+	return(ifihead);	/* pointer to first structure in linked list */
+}
+/* end get_ifa_info3 */
+
+/* include free_ifa_info */
+void
+freeifaddrs(struct ifa_info *ifihead)
+{
+	struct ifa_info	*ifi, *ifinext;
+
+	for (ifi = ifihead; ifi != NULL; ifi = ifinext) {
+		if (ifi->ifa_addr != NULL)
+			free(ifi->ifa_addr);
+		if (ifi->ifa_brdaddr != NULL)
+			free(ifi->ifa_brdaddr);
+		if (ifi->ifa_dstaddr != NULL)
+			free(ifi->ifa_dstaddr);
+		ifinext = ifi->ifa_next;	/* can't fetch ifa_next after free() */
+		free(ifi);					/* the ifa_info{} itself */
+	}
+}
+/* end free_ifa_info */
+
+struct ifa_info *
+Get_ifa_info(int family, int doaliases)
+{
+	struct ifa_info	*ifi;
+
+	if ( (ifi = get_ifa_info(family, doaliases)) == NULL)
+		debugf("get_ifa_info error");
+	return(ifi);
+}
+
+mDNSlocal int getifaddrs(struct ifa_info **ifalist)
+	{
+	*ifalist = get_ifa_info(PF_INET, false);
+	if( ifalist == nil )
+		return -1;
+	else
+		return(0);
+	}
+
+mDNSlocal mStatus SetupInterface(mDNS *const m, NetworkInterfaceInfo2 *info, struct ifa_info *ifa)
+	{
+	mStatus err = 0;
+	struct sockaddr_in *ifa_addr = (struct sockaddr_in *)ifa->ifa_addr;
+	CFSocketContext myCFSocketContext = { 0, info, NULL, NULL, NULL };
+
+	info->ifinfo.ip.NotAnInteger = ifa_addr->sin_addr.s_addr;
+	info->ifinfo.Advertise       = mDNStrue;
+	info->m         = m;
+	info->ifa_name  = (char *) malloc(strlen(ifa->ifa_name) + 1);
+	if (!info->ifa_name) return(-1);
+	strcpy(info->ifa_name, ifa->ifa_name);
+	info->alias     = SearchForInterfaceByName(m, ifa->ifa_name);
+	info->socket1   = 0;
+	info->socket2   = 0;
+	info->cfsocket1 = 0;
+	info->cfsocket2 = 0;
+
+	mDNS_RegisterInterface(m, &info->ifinfo);
+
+	if (info->alias)
+		debugf("SetupInterface: %s Flags %04X %.4a is an alias of %.4a",
+			ifa->ifa_name, ifa->ifa_flags, &info->ifinfo.ip, &info->alias->ifinfo.ip);
+
+	err = SetupSocket(ifa_addr, MulticastDNSPort, &info->socket1, &info->cfsocket1, &myCFSocketContext);
+
+	debugf("SetupInterface: %s Flags %04X %.4a Registered",
+		ifa->ifa_name, ifa->ifa_flags, &info->ifinfo.ip);
+
+	return(err);
+	}
+
+#else
+
 mDNSlocal mStatus SetupInterface(mDNS *const m, NetworkInterfaceInfo2 *info, struct ifaddrs *ifa)
 	{
 	extern int use_53;
@@ -321,6 +570,8 @@ mDNSlocal mStatus SetupInterface(mDNS *const m, NetworkInterfaceInfo2 *info, str
 	return(err);
 	}
 
+#endif
+
 mDNSlocal void ClearInterfaceList(mDNS *const m)
 	{
 	while (m->HostInterfaces)
@@ -338,9 +589,15 @@ mDNSlocal void ClearInterfaceList(mDNS *const m)
 
 mDNSlocal mStatus SetupInterfaceList(mDNS *const m)
 	{
+#if RUN_ON_PUMA_WITHOUT_IFADDRS
+	struct ifa_info *ifalist;
+	int err = getifaddrs(&ifalist);
+	struct ifa_info *ifa = ifalist;
+#else
 	struct ifaddrs *ifalist;
 	int err = getifaddrs(&ifalist);
 	struct ifaddrs *ifa = ifalist;
+#endif
 	if (err) return(err);
 
 	// Set up the nice label
