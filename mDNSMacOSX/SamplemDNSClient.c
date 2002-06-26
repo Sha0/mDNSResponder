cf935174728bc185b9d65012d1482b0e7381072a
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2002-06-26 23:16:36 +0000

    Bug #: 2909792 mDNSResponder: Needs APIs to add and remove additional records Added -U option to test tool for testing record updates
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@251 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSMacOSX/SamplemDNSClient.c
+++ mDNSMacOSX/SamplemDNSClient.c
@@ -20,15 +20,77 @@
  * @APPLE_LICENSE_HEADER_END@
  */
 
+/*
+ * Formatting notes:
+ * This code follows the "Whitesmiths style" C indentation rules. Plenty of discussion
+ * on C indentation can be found on the web, such as <http://www.kafejo.com/komp/1tbs.htm>,
+ * but for the sake of brevity here I will say just this: Curly braces are not syntactially
+ * part of an "if" statement; they are the beginning and ending markers of a compound statement;
+ * therefore common sense dictates that if they are part of a compound statement then they
+ * should be indented to the same level as everything else in that compound statement.
+ * Indenting curly braces at the same level as the "if" implies that curly braces are
+ * part of the "if", which is false. (This is as misleading as people who write "char* x,y;"
+ * thinking that variables x and y are both of type "char*" -- and anyone who doesn't
+ * understand why variable y is not of type "char*" just proves the point that poor code
+ * layout leads people to unfortunate misunderstandings about how the C language really works.)
+ */
+
 #include <libc.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <DNSServiceDiscovery/DNSServiceDiscovery.h>
+
+//*************************************************************************************************************
+// Globals
+//
+
+static dns_service_discovery_ref client = NULL;
+static char updatetest;
+
+//*************************************************************************************************************
+// Supporting Utility Functions
+//
+// This code takes care of:
+// 1. Extracting the mach_port_t from the dns_service_discovery_ref
+// 2. Making a CFMachPortRef from it
+// 3. Making a CFRunLoopSourceRef from that
+// 4. Adding that source to the current RunLoop
+// 5. and passing the resulting messages back to DNSServiceDiscovery_handleReply() for processing
+//
+// Code that's not based around a CFRunLoop will need its own mechanism to receive Mach messages
+// from the mDNSResponder daemon and pass them to the DNSServiceDiscovery_handleReply() routine.
+// (There is no way to automate this, because it varies depending on the application's existing
+// event handling model.)
+
+static void MyHandleMachMessage(CFMachPortRef port, void *msg, CFIndex size, void *info)
+	{
+	DNSServiceDiscovery_handleReply(msg);
+	}
+
+static int AddDNSServiceClientToRunLoop(dns_service_discovery_ref client)
+    {
+	mach_port_t port = DNSServiceDiscoveryMachPort(client);
+    if (!port)
+        return(-1);
+    else
+        {
+        CFMachPortContext  context    = { 0, 0, NULL, NULL, NULL };
+        Boolean            shouldFreeInfo;
+        CFMachPortRef      cfMachPort = CFMachPortCreateWithPort(kCFAllocatorDefault, port, MyHandleMachMessage, &context, &shouldFreeInfo);
+        CFRunLoopSourceRef rls        = CFMachPortCreateRunLoopSource(NULL, cfMachPort, 0);
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), rls, kCFRunLoopDefaultMode);
+        CFRelease(rls);
+        return(0);
+        }
+    }
+
+//*************************************************************************************************************
+// Sample callback functions for each of the operation types
 
 #define DomainMsg(X) ((X) == DNSServiceDomainEnumerationReplyAddDomain        ? "Added"     :          \
                       (X) == DNSServiceDomainEnumerationReplyAddDomainDefault ? "(Default)" :          \
                       (X) == DNSServiceDomainEnumerationReplyRemoveDomain     ? "Removed"   : "Unknown")
 
-void regdom_reply(DNSServiceDomainEnumerationReplyResultType resultType, const char *replyDomain,
+static void regdom_reply(DNSServiceDomainEnumerationReplyResultType resultType, const char *replyDomain,
     DNSServiceDiscoveryReplyFlags flags, void *context)
 	{
 	printf("Recommended Registration Domain %s %s", replyDomain, DomainMsg(resultType));
@@ -36,7 +98,7 @@ void regdom_reply(DNSServiceDomainEnumerationReplyResultType resultType, const c
 	printf("\n");
 	}
 
-void browsedom_reply(DNSServiceDomainEnumerationReplyResultType resultType, const char *replyDomain,
+static void browsedom_reply(DNSServiceDomainEnumerationReplyResultType resultType, const char *replyDomain,
     DNSServiceDiscoveryReplyFlags flags, void *context)
 	{
 	printf("Recommended Browsing Domain %s %s", replyDomain, DomainMsg(resultType));
@@ -44,7 +106,7 @@ void browsedom_reply(DNSServiceDomainEnumerationReplyResultType resultType, cons
 	printf("\n");
 	}
 
-void browse_reply(DNSServiceBrowserReplyResultType resultType,
+static void browse_reply(DNSServiceBrowserReplyResultType resultType,
     const char *replyName, const char *replyType, const char *replyDomain, DNSServiceDiscoveryReplyFlags flags, void *context)
 	{
 	char *op = (resultType == DNSServiceBrowserReplyAddInstance) ? "Found" : "Removed";
@@ -53,7 +115,7 @@ void browse_reply(DNSServiceBrowserReplyResultType resultType,
 	printf("\n");
 	}
 
-void resolve_reply(struct sockaddr *interface, struct sockaddr *address, const char *txtRecord, DNSServiceDiscoveryReplyFlags flags, void *context)
+static void resolve_reply(struct sockaddr *interface, struct sockaddr *address, const char *txtRecord, DNSServiceDiscoveryReplyFlags flags, void *context)
 	{
 	if (address->sa_family != AF_INET)
 		printf("Unknown address family %d\n", address->sa_family);
@@ -70,95 +132,107 @@ void resolve_reply(struct sockaddr *interface, struct sockaddr *address, const c
 		}
 	}
 
-void reg_reply(DNSServiceRegistrationReplyErrorType errorCode, void *context)
+static void myCFRunLoopTimerCallBack(CFRunLoopTimerRef timer, void *info)
+	{
+	(void)timer;	// Parameter not used
+	(void)info;		// Parameter not used
+    if (updatetest == 'Z') updatetest = 'A';
+    else updatetest++;
+    
+    printf("Updating Test TXT record to %c\n", updatetest);
+    DNSServiceRegistrationUpdateRecord(client, 0, 0, sizeof(updatetest), &updatetest);
+	}
+
+static void reg_reply(DNSServiceRegistrationReplyErrorType errorCode, void *context)
 	{
     printf("Got a reply from the server: ");
     switch (errorCode)
         {
         case kDNSServiceDiscoveryNoError:      printf("Name now registered and active\n"); break;
         case kDNSServiceDiscoveryNameConflict: printf("Name in use, please choose another\n"); exit(-1);
-        default:                               printf("Error %d\n", errorCode); break;
+        default:                               printf("Error %d\n", errorCode); return;
+        }
+
+    if (updatetest)
+        {
+        CFRunLoopTimerContext myCFRunLoopTimerContext = { 0, 0, NULL, NULL, NULL };
+        CFRunLoopTimerRef timer = CFRunLoopTimerCreate(kCFAllocatorDefault,
+            CFAbsoluteTimeGetCurrent() + 5.0, 5.0, 0, 1,	// Next fire time, periodic interval, flags, and order
+                                myCFRunLoopTimerCallBack, &myCFRunLoopTimerContext);
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode);
         }
 	}
 
-void MyHandleMachMessage(CFMachPortRef port, void *msg, CFIndex size, void *info)
-	{
-	DNSServiceDiscovery_handleReply(msg);
-	}
+//*************************************************************************************************************
+// The main test function
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 	{
 	char ch;
 	char *dom;
-	dns_service_discovery_ref client = NULL;
-	mach_port_t               port   = NULL;
 
 	if (argc < 2) goto Fail;		// Minimum command line is the command name and one argument
+    ch = getopt(argc, (char * const *)argv, "EFBLRU");
+	if (ch == -1) goto Fail;
 
-	while ((ch = getopt(argc, (char * const *)argv, "ERFBL")) != -1)
-		{
-		switch (ch)
-			{
-			case 'E':	printf("Looking for recommended registration domains:\n");
-						client = DNSServiceDomainEnumerationCreate(1, regdom_reply, nil);
-						break;
+    switch (ch)
+        {
+        case 'E':	printf("Looking for recommended registration domains:\n");
+                    client = DNSServiceDomainEnumerationCreate(1, regdom_reply, nil);
+                    break;
 
-			case 'F':	printf("Looking for recommended browsing domains:\n");
-						client = DNSServiceDomainEnumerationCreate(0, browsedom_reply, nil);
-						break;
+        case 'F':	printf("Looking for recommended browsing domains:\n");
+                    client = DNSServiceDomainEnumerationCreate(0, browsedom_reply, nil);
+                    break;
 
-			case 'B':	if (argc < optind+1) goto Fail;
-						dom = (argc < optind+2) ? "" : argv[optind+1];
-                        if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
-						printf("Browsing for %s%s\n", argv[optind+0], dom);
-						client = DNSServiceBrowserCreate(argv[optind+0], dom, browse_reply, nil);
-						break;
+        case 'B':	if (argc < optind+1) goto Fail;
+                    dom = (argc < optind+2) ? "" : argv[optind+1];
+                    if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
+                    printf("Browsing for %s%s\n", argv[optind+0], dom);
+                    client = DNSServiceBrowserCreate(argv[optind+0], dom, browse_reply, nil);
+                    break;
 
-			case 'L':	if (argc < optind+2) goto Fail;
-						dom = (argc < optind+3) ? "" : argv[optind+2];
-                        if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
-						printf("Lookup %s.%s%s\n", argv[optind+0], argv[optind+1], dom);
-						client = DNSServiceResolverResolve(argv[optind+0], argv[optind+1], dom, resolve_reply, nil);
-						break;
+        case 'L':	if (argc < optind+2) goto Fail;
+                    dom = (argc < optind+3) ? "" : argv[optind+2];
+                    if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
+                    printf("Lookup %s.%s%s\n", argv[optind+0], argv[optind+1], dom);
+                    client = DNSServiceResolverResolve(argv[optind+0], argv[optind+1], dom, resolve_reply, nil);
+                    break;
 
-			case 'R':	if (argc < optind+4) goto Fail;
-						{
-                        char *nam = argv[optind+0];
-                        char *typ = argv[optind+1];
-                        char *dom = argv[optind+2];
-						uint16_t PortAsNumber = atoi(argv[optind+3]);
-						Opaque16 registerPort = { { PortAsNumber >> 8, PortAsNumber & 0xFF } };
-						char *txt = (argc > optind+4) ? argv[optind+4] : "";
-                        if (nam[0] == '.' && nam[1] == 0) nam[0] = 0;	// We allow '.' on the command line as a synonym for empty string
-                        if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
-						printf("Registering Service %s.%s%s port %s %s\n", nam, typ, dom, argv[optind+3], txt);
-						client = DNSServiceRegistrationCreate(nam, typ, dom, registerPort, txt, reg_reply, nil);
-						break;
-						}
+        case 'R':	if (argc < optind+4) goto Fail;
+                    {
+                    char *nam = argv[optind+0];
+                    char *typ = argv[optind+1];
+                    char *dom = argv[optind+2];
+                    uint16_t PortAsNumber = atoi(argv[optind+3]);
+                    Opaque16 registerPort = { { PortAsNumber >> 8, PortAsNumber & 0xFF } };
+                    char *txt = (argc > optind+4) ? argv[optind+4] : "";
+                    if (nam[0] == '.' && nam[1] == 0) nam[0] = 0;	// We allow '.' on the command line as a synonym for empty string
+                    if (dom[0] == '.' && dom[1] == 0) dom[0] = 0;	// We allow '.' on the command line as a synonym for empty string
+                    printf("Registering Service %s.%s%s port %s %s\n", nam, typ, dom, argv[optind+3], txt);
+                    client = DNSServiceRegistrationCreate(nam, typ, dom, registerPort, txt, reg_reply, nil);
+                    break;
+                    }
 
-			default: goto Exit;
-			}
+        case 'U':	{
+                    Opaque16 registerPort = { { 0x12, 0x34 } };
+                    updatetest = 'A';
+                    printf("Registering Service Test._testupdate._tcp.local.\n");
+                    client = DNSServiceRegistrationCreate("Test", "_testupdate._tcp.", "", registerPort, "A", reg_reply, nil);
+                    break;
+                    }
 
-		if (client) port = DNSServiceDiscoveryMachPort(client);
-		if (!port)
-			{
-            fprintf(stderr, "Could not connect to mDNSResponder daemon\n");
-            return (-1);
-            }
-		else
-			{
-			CFMachPortContext context = { 0, 0, NULL, NULL, NULL };
-			Boolean           shouldFreeInfo;
-			CFMachPortRef cfMachPort = CFMachPortCreateWithPort(kCFAllocatorDefault, port, MyHandleMachMessage, &context, &shouldFreeInfo);
-			CFRunLoopSourceRef rls = CFMachPortCreateRunLoopSource(NULL, cfMachPort, 0);
-			CFRunLoopAddSource(CFRunLoopGetCurrent(), rls, kCFRunLoopDefaultMode);
-			CFRelease(rls);
-			printf("Talking to DNS SD Daemon at Mach port %d\n", port);
-			}
-		}
+        default: goto Exit;
+        }
+
+    if (!client) { fprintf(stderr, "DNSService call failed\n"); return (-1); }
+    if (AddDNSServiceClientToRunLoop(client) != 0) { fprintf(stderr, "AddDNSServiceClientToRunLoop failed\n"); return (-1); }
+    printf("Talking to DNS SD Daemon at Mach port %d\n", DNSServiceDiscoveryMachPort(client));
 	CFRunLoopRun();
     
     // Be sure to deallocate the dns_service_discovery_ref when you're finished
+    // Note: What other cleanup has to be done here?
+    // We should probably invalidate, remove and release our CFRunLoopSourceRef?
     DNSServiceDiscoveryDeallocate(client);
     
 Exit:
@@ -170,5 +244,8 @@ Fail:
 	fprintf(stderr, "%s -B        <Type> <Domain>   (Browse for Services Instances)\n", argv[0]);
 	fprintf(stderr, "%s -L <Name> <Type> <Domain>      (Look Up a Service Instance)\n", argv[0]);
 	fprintf(stderr, "%s -R <Name> <Type> <Domain> <Port> <TXT> (Register a Service)\n", argv[0]);
+	fprintf(stderr, "%s -U                             (Test Updating a TXT record)\n", argv[0]);
+	fprintf(stderr, "%s -A            (Test Adding a record -- not yet implemented)\n", argv[0]);
+	fprintf(stderr, "%s -D          (Test Deleting a record -- not yet implemented)\n", argv[0]);
 	return 0;
 	}
