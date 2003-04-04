0cd2dea2f809882396b2e6314518e91f445b9459
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2003-04-04 01:37:14 +0000

    Added NetMonitor.c
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@717 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSPosix/NetMonitor.c
@@ -0,0 +1,327 @@
+/*
+ * Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.2 (the
+ * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ * 
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ *
+ * Formatting notes:
+ * This code follows the "Whitesmiths style" C indentation rules. Plenty of discussion
+ * on C indentation can be found on the web, such as <http://www.kafejo.com/komp/1tbs.htm>,
+ * but for the sake of brevity here I will say just this: Curly braces are not syntactially
+ * part of an "if" statement; they are the beginning and ending markers of a compound statement;
+ * therefore common sense dictates that if they are part of a compound statement then they
+ * should be indented to the same level as everything else in that compound statement.
+ * Indenting curly braces at the same level as the "if" implies that curly braces are
+ * part of the "if", which is false. (This is as misleading as people who write "char* x,y;"
+ * thinking that variables x and y are both of type "char*" -- and anyone who doesn't
+ * understand why variable y is not of type "char*" just proves the point that poor code
+ * layout leads people to unfortunate misunderstandings about how the C language really works.)
+ *
+ * $Log: NetMonitor.c,v $
+ * Revision 1.1  2003/04/04 01:37:14  cheshire
+ * Added NetMonitor.c
+ *
+ */
+
+//*************************************************************************************************************
+// Incorporate mDNS.c functionality
+
+// We want to use much of the functionality provided by "mDNS.c",
+// except we'll steal the packets that would be sent to normal mDNSCoreReceive() routine
+#define mDNSCoreReceive __NOT__mDNSCoreReceive__NOT__
+#include "mDNS.c"
+#undef mDNSCoreReceive
+
+//*************************************************************************************************************
+// Headers
+
+#include <stdio.h>			// For printf()
+#include <stdlib.h>			// For malloc()
+
+#include "mDNSPosix.h"      // Defines the specific types needed to run mDNS on this platform
+#include "ExampleClientApp.h"
+
+//*************************************************************************************************************
+// Types and structures
+
+enum
+	{
+	OP_bprobe = 0,
+	OP_bquery,
+	OP_banswer,
+	OP_rprobe,
+	OP_rquery,
+	OP_ranswer,
+	NumStatOps
+	};
+
+typedef struct ActivityStat_struct ActivityStat;
+struct ActivityStat_struct
+	{
+	ActivityStat *next;
+	domainname srvtype;
+	int printed;
+	int totalops;
+	int stat[NumStatOps];
+	};
+
+//*************************************************************************************************************
+// Globals
+
+static mDNS mDNSStorage;						// mDNS core uses this to store its globals
+static mDNS_PlatformSupport PlatformStorage;	// Stores this platform's globals
+
+static int NumQuestions;
+static int NumProbes;
+static int NumAnswers;
+static int NumAdditionals;
+
+static ActivityStat *stats;
+
+//*************************************************************************************************************
+// Utilities
+
+// Special version of printf that knows how to print IP addresses, DNS-format name strings, etc.
+mDNSexport void mprintf(const char *format, ...)
+	{
+	unsigned char buffer[512];
+	va_list ptr;
+	va_start(ptr,format);
+	buffer[mDNS_vsprintf((char *)buffer, format, ptr)] = 0;
+	va_end(ptr);
+	printf("%s", buffer);
+	}
+
+//*************************************************************************************************************
+// Receive and process packets
+
+mDNSexport mDNSBool ExtractServiceType(const domainname *const fqdn, domainname *const srvtype)
+	{
+	int i, len;
+	const mDNSu8 *src = fqdn->c;
+	mDNSu8 *dst = srvtype->c;
+	
+	len = *src;
+	if (len == 0 || len >= 0x40) return(mDNSfalse);
+	if (src[1] != '_') src += 1 + len;
+	
+	len = *src;
+	if (len == 0 || len >= 0x40 || src[1] != '_') return(mDNSfalse);
+	for (i=0; i<=len; i++) *dst++ = *src++;
+
+	len = *src;
+	if (len == 0 || len >= 0x40 || src[1] != '_') return(mDNSfalse);
+	for (i=0; i<=len; i++) *dst++ = *src++;
+
+	*dst++ = 0;		// Put the null root label on the end of the service type
+
+	return(mDNStrue);
+	}
+
+mDNSlocal void recordstat(domainname *fqdn, int op, mDNSu16 rrtype)
+	{
+	ActivityStat **s = &stats;
+	domainname srvtype;
+
+	if (rrtype == kDNSType_PTR) op += OP_bquery;
+	else if (rrtype == kDNSType_SRV || rrtype == kDNSType_TXT) op += OP_rprobe;
+	else return;
+	
+	if (!ExtractServiceType(fqdn, &srvtype)) return;
+
+	while (*s && !SameDomainName(&(*s)->srvtype, &srvtype)) s = &(*s)->next;
+	if (!*s)
+		{
+		int i;
+		*s = malloc(sizeof(ActivityStat));
+		if (!*s) exit(-1);
+		(*s)->next     = NULL;
+		(*s)->srvtype  = srvtype;
+		(*s)->printed  = 0;
+		(*s)->totalops = 0;
+		for (i=0; i<NumStatOps; i++) (*s)->stat[i] = 0;
+		}
+
+	(*s)->totalops++;
+	(*s)->stat[op]++;
+	}
+
+mDNSlocal void printstats(void)
+	{
+	int i;
+	mprintf("%-30s    Total Ops  Probe Q   Probe A  Browse Q  Browse A Resolve Q Resolve A\n", "Service Type");
+
+	for (i=0; i<20; i++) 
+		{
+		int max = 0;
+		ActivityStat *s, *m = NULL;
+		for (s = stats; s; s=s->next)
+			if (!s->printed && max < s->totalops)
+				{ m = s; max = s->totalops; }
+		if (!m) return;
+		m->printed = mDNStrue;
+		mprintf("%##-30s %9d %9d %9d %9d %9d %9d %9d\n", &m->srvtype, m->totalops, m->stat[0], m->stat[1], m->stat[2], m->stat[3], m->stat[4], m->stat[5]);
+		//mprintf("%9d %9d %9d %9d %9d\n", m->totalops, m->stat[0], m->stat[1], m->stat[2], m->stat[3]);
+		}
+	}
+
+mDNSlocal mDNSBool FindUpdate(const DNSMessage *const query, const mDNSu8 *ptr, const mDNSu8 *const end, DNSQuestion *q, ResourceRecord *pktrr)
+	{
+	int i;
+	for (i = 0; i < query->h.numAuthorities; i++)
+		{
+		ptr = getResourceRecord(query, ptr, end, q->InterfaceID, 0, 0, pktrr, mDNSNULL);
+		if (!ptr) break;
+		if (ResourceRecordAnswersQuestion(pktrr, q)) return(mDNStrue);
+		}
+	return(mDNSfalse);
+	}
+
+mDNSlocal void DisplayQuery(const DNSMessage *const msg, const mDNSu8 *const end, const mDNSAddr *srcaddr, const mDNSInterfaceID InterfaceID)
+	{
+	int i;
+	const mDNSu8 *ptr = msg->data;
+	const mDNSu8 *auth = LocateAuthorities(msg, end);
+	ResourceRecord pktrr;
+
+	mprintf("\n");
+	mprintf("%#-16a -Q- Q:%3d  Ans:%3d  Auth:%3d  Add:%3d\n", srcaddr, msg->h.numQuestions, msg->h.numAnswers, msg->h.numAuthorities, msg->h.numAdditionals);
+
+	for (i=0; i<msg->h.numQuestions; i++)
+		{
+		DNSQuestion q;
+		ptr = getQuestion(msg, ptr, end, InterfaceID, &q);
+		if (!ptr) { mprintf("%#-16a **** ERROR: FAILED TO READ QUESTION **** \n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c); return; }
+		if (FindUpdate(msg, auth, end, &q, &pktrr))
+			{
+			NumProbes++;
+			mprintf("%#-16a (P)  %-5s %##s\n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c);
+			recordstat(&q.name, OP_bprobe, q.rrtype);
+			}
+		else
+			{
+			NumQuestions++;
+			mprintf("%#-16a (Q)  %-5s %##s\n", srcaddr, DNSTypeName(q.rrtype), q.name.c);
+			recordstat(&q.name, OP_bquery, q.rrtype);
+			}
+		}
+
+	for (i=0; i<msg->h.numAnswers; i++)
+		{
+		ptr = getResourceRecord(msg, ptr, end, InterfaceID, 0, 0, &pktrr, mDNSNULL);
+		mprintf("%#-16a (KA) %-5s %##s", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c);
+		if (pktrr.rrtype == kDNSType_PTR) mprintf("-> %##s", pktrr.rdata->u.name.c);
+		mprintf("\n");
+		}
+	}
+
+mDNSlocal void DisplayResponse(const DNSMessage *const msg, const mDNSu8 *end, const mDNSAddr *srcaddr, const mDNSInterfaceID InterfaceID)
+	{
+	int i;
+	const mDNSu8 *ptr = msg->data;
+	ResourceRecord pktrr;
+
+	mprintf("\n");
+	mprintf("%#-16a -R- Q:%3d  Ans:%3d  Auth:%3d  Add:%3d\n", srcaddr, msg->h.numQuestions, msg->h.numAnswers, msg->h.numAuthorities, msg->h.numAdditionals);
+
+	for (i=0; i<msg->h.numQuestions; i++)
+		{
+		DNSQuestion q;
+		ptr = getQuestion(msg, ptr, end, InterfaceID, &q);
+		if (!ptr) { mprintf("%#-16a **** ERROR: FAILED TO READ QUESTION **** \n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c); return; }
+		mprintf("%#-16a (?)  **** ERROR: SHOULD NOT HAVE Q IN mDNS RESPONSE **** %-5s %##s\n", srcaddr, DNSTypeName(q.rrtype), q.name.c);
+		}
+
+	for (i=0; i<msg->h.numAnswers; i++)
+		{
+		ptr = getResourceRecord(msg, ptr, end, InterfaceID, 0, 0, &pktrr, mDNSNULL);
+		if (!ptr) { mprintf("%#-16a **** ERROR: FAILED TO READ ANSWER **** \n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c); return; }
+		NumAnswers++;
+		mprintf("%#-16a (AN) %-5s %##s", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c);
+		if (pktrr.rrtype == kDNSType_PTR) mprintf("-> %##s", pktrr.rdata->u.name.c);
+		mprintf("\n");
+		recordstat(&pktrr.name, OP_banswer, pktrr.rrtype);
+		}
+
+	for (i=0; i<msg->h.numAuthorities; i++)
+		{
+		ptr = getResourceRecord(msg, ptr, end, InterfaceID, 0, 0, &pktrr, mDNSNULL);
+		if (!ptr) { mprintf("%#-16a **** ERROR: FAILED TO READ AUTHORITY **** \n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c); return; }
+		mprintf("%#-16a (?)  **** ERROR: SHOULD NOT HAVE AUTHORITY IN mDNS RESPONSE **** %-5s %##s\n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c);
+		}
+
+	for (i=0; i<msg->h.numAdditionals; i++)
+		{
+		ptr = getResourceRecord(msg, ptr, end, InterfaceID, 0, 0, &pktrr, mDNSNULL);
+		if (!ptr) { mprintf("%#-16a **** ERROR: FAILED TO READ ADDITIONAL **** \n", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c); return; }
+		NumAdditionals++;
+		mprintf("%#-16a (AD) %-5s %##s", srcaddr, DNSTypeName(pktrr.rrtype), pktrr.name.c);
+		if (pktrr.rrtype == kDNSType_PTR) mprintf("-> %##s", pktrr.rdata->u.name.c);
+		mprintf("\n");
+		}
+
+	}
+
+mDNSexport void mDNSCoreReceive(mDNS *const m, DNSMessage *const msg, const mDNSu8 *const end,
+	const mDNSAddr *srcaddr, mDNSIPPort srcport, const mDNSAddr *dstaddr, mDNSIPPort dstport, const mDNSInterfaceID InterfaceID)
+	{
+	// For now we're only interested in monitoring IPv4 traffic.
+	// All IPv6 packets should just be duplicates of the v4 packets.
+	if (srcaddr->type == mDNSAddrType_IPv4)
+		{
+		const mDNSu8 StdQ = kDNSFlag0_QR_Query    | kDNSFlag0_OP_StdQuery;
+		const mDNSu8 StdR = kDNSFlag0_QR_Response | kDNSFlag0_OP_StdQuery;
+		const mDNSu8 QR_OP = (mDNSu8)(msg->h.flags.b[0] & kDNSFlag0_QROP_Mask);
+		
+		// Read the integer parts which are in IETF byte-order (MSB first, LSB second)
+		mDNSu8 *ptr = (mDNSu8 *)&msg->h.numQuestions;
+		msg->h.numQuestions   = (mDNSu16)((mDNSu16)ptr[0] <<  8 | ptr[1]);
+		msg->h.numAnswers     = (mDNSu16)((mDNSu16)ptr[2] <<  8 | ptr[3]);
+		msg->h.numAuthorities = (mDNSu16)((mDNSu16)ptr[4] <<  8 | ptr[5]);
+		msg->h.numAdditionals = (mDNSu16)((mDNSu16)ptr[6] <<  8 | ptr[7]);
+		
+		mDNS_Lock(m);
+		if      (QR_OP == StdQ) DisplayQuery   (msg, end, srcaddr, InterfaceID);
+		else if (QR_OP == StdR) DisplayResponse(msg, end, srcaddr, InterfaceID);
+		else debugf("Unknown DNS packet type %02X%02X (ignored)", msg->h.flags.b[0], msg->h.flags.b[1]);
+		mDNS_Unlock(m);
+		}
+	}
+
+mDNSexport int main(int argc, char **argv)
+	{
+	mStatus status = mDNS_Init(&mDNSStorage, &PlatformStorage,
+		mDNS_Init_NoCache, mDNS_Init_ZeroCacheSize,
+		mDNS_Init_DontAdvertiseLocalAddresses,
+		mDNS_Init_NoInitCallback, mDNS_Init_NoInitCallbackContext);
+	if (status) { fprintf(stderr, "%s: mDNS_Init failed %ld\n", argv[0], status); return(status); }
+
+	ExampleClientEventLoop(&mDNSStorage);
+
+	mprintf("\n\n");
+	
+	mprintf("Total Query Questions:      %6d\n", NumQuestions);
+	mprintf("Total Probe Questions:      %6d\n", NumProbes);
+	mprintf("Total Answers/Announcements:%6d\n", NumAnswers);
+	mprintf("Total Additional Records:   %6d\n", NumAdditionals);
+	
+	printstats();
+
+	mDNS_Close(&mDNSStorage);
+	return(0);
+	}
