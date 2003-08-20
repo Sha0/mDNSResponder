5d717fd0f18764f37d538286e963726c239b625d
bradley <bradley@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2003-08-20 06:01:19 +0000

    Prefix files for CodeWarrior Windows builds to set compile options.
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@1294 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSWindows/Applications/DNSServiceTest/ToolPrefixWindows.h
@@ -0,0 +1,19 @@
+/*
+	Contains:	Rendezvous Test Tool
+
+	Copyright:	(C) 2003 Apple Computer, Inc., all rights reserved.
+
+	Change History (most recent first):
+
+		$Log: ToolPrefixWindows.h,v $
+		Revision 1.1  2003/08/20 06:01:19  bradley
+		Prefix files for CodeWarrior Windows builds to set compile options.
+						
+*/
+
+#ifndef __TOOL_PREFIX_WINDOWS__
+#define __TOOL_PREFIX_WINDOWS__
+
+#define	DEBUG		0
+
+#endif	// __TOOL_PREFIX_WINDOWS__
---
+++ mDNSWindows/Applications/DNSServiceTest/ToolPrefixWindowsDebug.h
@@ -0,0 +1,20 @@
+/*
+	Contains:	Rendezvous Test Tool
+
+	Copyright:	(C) 2003 Apple Computer, Inc., all rights reserved.
+
+	Change History (most recent first):
+
+		$Log: ToolPrefixWindowsDebug.h,v $
+		Revision 1.1  2003/08/20 06:01:19  bradley
+		Prefix files for CodeWarrior Windows builds to set compile options.
+					
+*/
+
+#ifndef __TOOL_PREFIX_WINDOWS_DEBUG__
+#define __TOOL_PREFIX_WINDOWS_DEBUG__
+
+#define	DEBUG				1
+#define	MDNS_DEBUGMSGS		1
+
+#endif	// __TOOL_PREFIX_WINDOWS_DEBUG__
