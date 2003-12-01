a7799a1ac563216a3d040df3f1b94b5bb848e100
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2003-12-01 21:47:44 +0000

    APSL
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@1480 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSPosix/parselog.py
+++ mDNSPosix/parselog.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python
 
 # parselog.py, written and contributed by Kevin Marks
-# Requires Panther ofr python and CG Python APIs
+#
+# Copyright (c) 2002-2003 Apple Computer, Inc. All rights reserved.
+#
+# @APPLE_LICENSE_HEADER_START@
+#
+# This file contains Original Code and/or Modifications of Original Code
+# as defined in and that are subject to the Apple Public Source License
+# Version 2.0 (the 'License'). You may not use this file except in
+# compliance with the License. Please obtain a copy of the License at
+# http://www.opensource.apple.com/apsl/ and read it before using this
+# file.
+#
+# The Original Code and all software distributed under the License are
+# distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+# EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+# INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+# Please see the License for the specific language governing rights and
+# limitations under the License.
+#
+# @APPLE_LICENSE_HEADER_END@
+#
+# Requires OS X 10.3 Panther for Python and Core Graphics Python APIs
 # Invoke from the command line with "parselog.py fname" where fname is a log file made by mDNSNetMonitor
 #
 # Caveats:
@@ -38,10 +60,11 @@
 # Filled green  circle: Normal answer             Hollow green  circle: Goodbye message (record going away)
 #                                                 Hollow blue   circle: Legacy query (from old client)
 # $Log: parselog.py,v $
+# Revision 1.2  2003/12/01 21:47:44  cheshire
+# APSL
+#
 # Revision 1.1  2003/10/10 02:14:17  cheshire
 # First checkin of parselog.py, a tool to create graphical representations of mDNSNetMonitor logs
-#
-# 
 
 from CoreGraphics import *
 import math   # for pi
