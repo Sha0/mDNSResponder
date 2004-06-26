e1142b982a57f1748516fa1bcdd6a6f2b8410073
rpantos <rpantos@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-06-26 19:17:41 +0000

    First checked in.
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2044 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSWindows/WinVersRes.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2004 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+
+    Change History (most recent first):
+
+$Log: WinVersRes.h,v $
+Revision 1.1  2004/06/26 19:17:41  rpantos
+First checked in.
+
+ */
+
+#ifndef WINRESVERS_H
+#define WINRESVERS_H
+
+#define MASTER_PROD_NAME	"Rendezvous"
+
+// Define the product version for mDNSResponder on Windows
+#define MASTER_PROD_VERS		1,0,0,6
+#define MASTER_PROD_VERS_STR	"1,0,0,6"
+
+#endif // WINRESVERS_H
