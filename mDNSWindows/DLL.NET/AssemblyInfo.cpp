c64c5f8844d93d816ec2d0b28851a5f3f582d2b9
shersche <shersche@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-07-14 15:42:47 +0000

    remove entries for strong authentication
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2096 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSWindows/DLL.NET/AssemblyInfo.cpp
+++ mDNSWindows/DLL.NET/AssemblyInfo.cpp
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: AssemblyInfo.cpp,v $
+Revision 1.2  2004/07/14 15:42:47  shersche
+remove entries for strong authentication
+
 Revision 1.1  2004/06/26 04:01:22  shersche
 Initial revision
 
@@ -59,31 +62,5 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
-
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//        When specifying the KeyFile, the location of the KeyFile should be
-//        relative to the project directory.
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-[assembly:AssemblyDelaySignAttribute(false)];
-[assembly:AssemblyKeyFileAttribute("dnssd_NET.snk")];
-[assembly:AssemblyKeyNameAttribute("")];
+[assembly:AssemblyVersionAttribute("1.0.0.8")];
 
