1efdddaf6222f5b32630ccc70c97381f5fc684a8
rpantos <rpantos@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-08-05 18:00:04 +0000

    bump to 1.0.0.13
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2163 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSWindows/WinVersRes.h
+++ mDNSWindows/WinVersRes.h
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: WinVersRes.h,v $
+Revision 1.10  2004/08/05 18:00:04  rpantos
+bump to 1.0.0.13
+
 Revision 1.9  2004/07/27 07:31:46  shersche
 bump to 1.0.0.12
 
@@ -58,9 +61,9 @@ First checked in.
 #define MASTER_PROD_NAME	"Rendezvous"
 
 // Define the product version for mDNSResponder on Windows
-#define MASTER_PROD_VERS		1,0,0,12
-#define MASTER_PROD_VERS_STR	"1,0,0,12"
-#define MASTER_PROD_VERS_STR2	"1.0.0.12"
-#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.12"
+#define MASTER_PROD_VERS		1,0,0,13
+#define MASTER_PROD_VERS_STR	"1,0,0,13"
+#define MASTER_PROD_VERS_STR2	"1.0.0.13"
+#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.13"
 
 #endif // WINRESVERS_H
