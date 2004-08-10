69d389c4e0c06f1505f93a793e9eaaec01745913
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-08-10 01:10:01 +0000

    <rdar://problem/3588761> Current method of doing subtypes causes name collisions Minor revision from Roger Pantos
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2170 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSCore/DNSCommon.c
+++ mDNSCore/DNSCommon.c
@@ -23,6 +23,10 @@
     Change History (most recent first):
 
 $Log: DNSCommon.c,v $
+Revision 1.41  2004/08/10 01:10:01  cheshire
+<rdar://problem/3588761> Current method of doing subtypes causes name collisions
+Minor revision from Roger Pantos
+
 Revision 1.40  2004/08/04 22:10:46  cheshire
 <rdar://problem/3588761> Current method of doing subtypes causes name collisions
 Change to use "._sub." instead of ".s." to mark subtypes.
@@ -643,22 +647,29 @@ mDNSexport mDNSu8 *ConstructServiceName(domainname *const fqdn,
 	// a single-label subtype is allowed as the first label of a three-part "type"
 	if (!name && type)
 		{
-		const mDNSu8 *s0 = type->c, *s1 = s0, *s2 = s0;
-		if (s0[0] && s0[0] < 0x40) s1 = s0 + 1 + s0[0];
-		if (s1[0] && s1[0] < 0x40) s2 = s1 + 1 + s1[0];
-		if (s2[0] && s2[0] < 0x40 && s2[1+s2[0]] == 0)	// Must have three and only three labels
+		const mDNSu8 *s0 = type->c;
+		if (s0[0] && s0[0] < 0x40)		// If legal first label (at least one character, and no more than 63)
 			{
-			src = s0;		// Copy the first label
-			len = *src;
-			for (i=0; i<=len; i++) *dst++ = *src++;
-			for (i=0; i<=4;   i++) *dst++ = "\x04_sub"[i];
-			type = (domainname *)s1;
-			
-			// Special support for queries done by older versions of "Rendezvous Browser"
-			// For these queries, we retract the ".s" we just added between the subtype and the main type
-			if (SameDomainName((domainname*)s0, (domainname*)"\x09_services\x07_dns-sd\x04_udp") ||
-				SameDomainName((domainname*)s0, (domainname*)"\x09_services\x05_mdns\x04_udp"))
-				dst -= 5;
+			const mDNSu8 * s1 = s0 + 1 + s0[0];
+			if (s1[0] && s1[0] < 0x40)	// and legal second label (at least one character, and no more than 63)
+				{
+				const mDNSu8 *s2 = s1 + 1 + s1[0];
+				if (s2[0] && s2[0] < 0x40 && s2[1+s2[0]] == 0)	// and we have three and only three labels
+					{
+					static const mDNSu8 SubTypeLabel[5] = "\x04_sub";
+					src = s0;									// Copy the first label
+					len = *src;
+					for (i=0; i <= len;                      i++) *dst++ = *src++;
+					for (i=0; i < (int)sizeof(SubTypeLabel); i++) *dst++ = SubTypeLabel[i];
+					type = (domainname *)s1;
+					
+					// Special support for queries done by older versions of "Rendezvous Browser"
+					// For these queries, we retract the "._sub" we just added between the subtype and the main type
+					if (SameDomainName((domainname*)s0, (domainname*)"\x09_services\x07_dns-sd\x04_udp") ||
+						SameDomainName((domainname*)s0, (domainname*)"\x09_services\x05_mdns\x04_udp"))
+						dst -= sizeof(SubTypeLabel);
+					}
+				}
 			}
 		}
 
