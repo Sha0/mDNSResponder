8c2747736415d218bbdb34995278b89f6a626089
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-10-14 23:38:04 +0000

    mDNSResponder-80
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2543 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@
 
 include /Developer/Makefiles/pb_makefiles/platform.make
 
-MVERS = "mDNSResponder-79"
+MVERS = "mDNSResponder-80"
 
 install:
 	cd "$(SRCROOT)/mDNSMacOSX"; xcodebuild install     OBJROOT=$(OBJROOT) SYMROOT=$(SYMROOT) DSTROOT=$(DSTROOT) MVERS=$(MVERS)
--- mDNSMacOS9/mDNSLibraryResources.r
+++ mDNSMacOS9/mDNSLibraryResources.r
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: mDNSLibraryResources.r,v $
+Revision 1.12  2004/10/14 23:38:04  cheshire
+mDNSResponder-80
+
 Revision 1.11  2004/10/07 21:49:15  cheshire
 mDNSResponder-79
 
@@ -74,15 +77,15 @@ like Muse Research who want to be able to use mDNS/DNS-SD from GPL-licensed code
 
 resource 'vers' (1, purgeable)
 	{
-   0x01, 0x00, alpha, 79, verUS,
-   "1.0a79",
-   "Multicast DNS & DNS Service Discovery 1.0a79"
+   0x01, 0x00, alpha, 80, verUS,
+   "1.0a80",
+   "Multicast DNS & DNS Service Discovery 1.0a80"
 	};
 
 resource 'vers' (2, purgeable)
 	{
-   0x01, 0x00, alpha, 79, verUS,
-   "1.0a79",
+   0x01, 0x00, alpha, 80, verUS,
+   "1.0a80",
    "developer.apple.com/darwin/projects/rendezvous/"
 	};
 
