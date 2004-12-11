844e7fa12ad8f36b4d32c462f92a50c11877334e
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2004-12-11 01:45:30 +0000

    Add kDNSServiceFlagsAllowRemoteQuery (commented out, for now)
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@2811 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- Clients/dns-sd.c
+++ Clients/dns-sd.c
@@ -549,7 +549,7 @@ static DNSServiceErrorType RegisterService(DNSServiceRef *sdRef,
 	printf("Registering Service %s.%s%s", nam, typ, dom);
 	if (host && *host) printf(" host %s", host);
 	printf(" port %s %s\n", port, txt);
-	return(DNSServiceRegister(sdRef, 0, 0, nam, typ, dom, host, registerPort.NotAnInteger, ptr-txt, txt, reg_reply, NULL));
+	return(DNSServiceRegister(sdRef, /* kDNSServiceFlagsAllowRemoteQuery */ 0, 0, nam, typ, dom, host, registerPort.NotAnInteger, ptr-txt, txt, reg_reply, NULL));
 	}
 
 int main(int argc, char **argv)
