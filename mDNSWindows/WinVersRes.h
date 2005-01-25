eed4a0490b49c591e27c7196edc1a2654c5b3061
shersche <shersche@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2005-01-25 17:15:52 +0000

    Bump to 1.0.0.51. Add legal copyright string.
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@3211 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSWindows/WinVersRes.h
+++ mDNSWindows/WinVersRes.h
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: WinVersRes.h,v $
+Revision 1.22  2005/01/25 17:15:52  shersche
+Bump to 1.0.0.51. Add legal copyright string.
+
 Revision 1.21  2005/01/11 07:09:32  shersche
 Bump to version 1.0.0.51
 
@@ -94,9 +97,12 @@ First checked in.
 #define MASTER_PROD_NAME	"Rendezvous"
 
 // Define the product version for mDNSResponder on Windows
-#define MASTER_PROD_VERS		1,0,0,51
-#define MASTER_PROD_VERS_STR	"1,0,0,51"
-#define MASTER_PROD_VERS_STR2	"1.0.0.51"
-#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.51"
+#define MASTER_PROD_VERS		1,0,0,52
+#define MASTER_PROD_VERS_STR	"1,0,0,52"
+#define MASTER_PROD_VERS_STR2	"1.0.0.52"
+#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.52"
+
+// Define the legal copyright
+#define MASTER_LEGAL_COPYRIGHT "Copyright (C) 2003-2005 Apple Computer, Inc."
 
 #endif // WINRESVERS_H
