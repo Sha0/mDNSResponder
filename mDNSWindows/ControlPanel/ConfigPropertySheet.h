db9853f3b5248bad0bb097b8054c38ea1e2e3c86
shersche <shersche@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2005-02-23 23:02:24 +0000

    <rdar://problem/3947424> Control Panel to configure Wide-Area Rendezvous
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@3398 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

---
+++ mDNSWindows/ControlPanel/ConfigPropertySheet.h
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2002-2004 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+
+    Change History (most recent first):
+
+*/
+
+#ifndef _ConfigPropertySheet_h
+#define _ConfigPropertySheet_h
+
+#include "stdafx.h"
+#include "FirstPage.h"
+#include "SecondPage.h"
+
+#include <dns_sd.h>
+#include <list>
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+//	CConfigPropertySheet
+//---------------------------------------------------------------------------------------------------------------------------
+
+class CConfigPropertySheet : public CPropertySheet
+{
+public:
+
+	CConfigPropertySheet();
+	virtual ~CConfigPropertySheet();
+
+	typedef std::list<CString> StringList;
+
+	StringList	m_browseDomains;
+	StringList	m_regDomains;
+
+protected:
+
+	CFirstPage	m_firstPage;
+	CSecondPage	m_secondPage;
+
+	//{{AFX_VIRTUAL(CConfigPropertySheet)
+	//}}AFX_VIRTUAL
+
+	DECLARE_DYNCREATE(CConfigPropertySheet)
+
+	//{{AFX_MSG(CConfigPropertySheet)
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+	afx_msg BOOL	OnInitDialog();
+	afx_msg BOOL	OnCommand( WPARAM wParam, LPARAM lParam );
+	afx_msg LONG	OnDataReady( WPARAM inWParam, LPARAM inLParam );
+	afx_msg LONG	OnRegistryChanged( WPARAM inWParam, LPARAM inLParam );
+	void			OnEndDialog();
+
+private:
+
+	OSStatus
+	SetupBrowsing();
+
+	OSStatus
+	TearDownBrowsing();
+
+	OSStatus
+	SetupRegistryNotifications();
+
+	OSStatus
+	TearDownRegistryNotifications();
+
+	OSStatus
+	DecodeDomainName( const char * raw, CString & decoded );
+
+	const char*
+	GetNextLabel( const char * cstr, char label[64] );
+
+	static void DNSSD_API
+	BrowseDomainsReply
+				(
+				DNSServiceRef			sdRef,
+				DNSServiceFlags			flags,
+				uint32_t				interfaceIndex,
+				DNSServiceErrorType		errorCode,
+				const char			*	replyDomain,
+				void				*	context
+				);
+
+	static void DNSSD_API
+	RegDomainsReply
+				(
+				DNSServiceRef			sdRef,
+				DNSServiceFlags			flags,
+				uint32_t				interfaceIndex,
+				DNSServiceErrorType		errorCode,
+				const char			*	replyDomain,
+				void				*	context
+				);
+
+	// This thread will watch for registry changes
+
+	static unsigned WINAPI
+	WatchRegistry
+				(
+				LPVOID inParam
+				);
+
+	HKEY				m_statusKey;
+	HANDLE				m_thread;
+	HANDLE				m_threadExited;
+	DNSServiceRef		m_browseDomainsRef;
+	DNSServiceRef		m_regDomainsRef;
+	CRITICAL_SECTION	m_lock;
+};
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+//	Registry Constants
+//---------------------------------------------------------------------------------------------------------------------------
+
+#define	kServiceName							L"Apple mDNSResponder"
+#define kServiceDynDNSBrowseDomains				L"BrowseDomains"
+#define kServiceDynDNSHostNames					L"HostNames"
+#define kServiceDynDNSRegistrationDomains		L"RegistrationDomains"
+#define kServiceDynDNSDomains					L"Domains"	// value is comma separated list of domains
+#define kServiceDynDNSEnabled					L"Enabled"
+#define kServiceDynDNSStatus					L"Status"
+
+#endif
---
+++ mDNSWindows/ControlPanel/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ControlPanel.rc
+//
+#define IDR_APPLET                      131
+#define IDR_APPLET_PAGE1                131
+#define IDS_APPLET_DESCRIPTION          132
+#define IDR_APPLET_PAGE2                132
+#define IDR_SECRET                      133
+#define IDI_FAILURE                     140
+#define IDI_SUCCESS                     141
+#define IDC_EDIT1                       1000
+#define IDC_BUTTON1                     1001
+#define IDC_COMBO1                      1002
+#define IDC_CHECK1                      1003
+#define IDC_COMBO2                      1004
+#define IDC_EDIT2                       1005
+#define IDC_SECRET                      1005
+#define IDC_COMBO3                      1007
+#define IDC_FAILURE                     1008
+#define IDC_SUCCESS                     1009
+#define IDC_SECRET_NAME                 1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
