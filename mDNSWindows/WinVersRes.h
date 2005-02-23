14e02f6f93f5cb2de1da6e7b9fc639f3fd1fbfde
shersche <shersche@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2005-02-23 03:12:27 +0000

    Bump to 1.0.0.56
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@3395 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSWindows/WinVersRes.h
+++ mDNSWindows/WinVersRes.h
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: WinVersRes.h,v $
+Revision 1.27  2005/02/23 03:12:27  shersche
+Bump to 1.0.0.56
+
 Revision 1.26  2005/02/15 23:20:18  shersche
 Bump to 1.0.0.55 and update name
 
@@ -109,10 +112,10 @@ First checked in.
 #define MASTER_PROD_NAME	"Rendezvous"
 
 // Define the product version for mDNSResponder on Windows
-#define MASTER_PROD_VERS		1,0,0,55
-#define MASTER_PROD_VERS_STR	"1,0,0,55"
-#define MASTER_PROD_VERS_STR2	"1.0.0.55"
-#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.55"
+#define MASTER_PROD_VERS		1,0,0,56
+#define MASTER_PROD_VERS_STR	"1,0,0,56"
+#define MASTER_PROD_VERS_STR2	"1.0.0.56"
+#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.0.56"
 
 // Define the legal copyright
 #define MASTER_LEGAL_COPYRIGHT "Copyright (C) 2003-2005 Apple Computer, Inc."
