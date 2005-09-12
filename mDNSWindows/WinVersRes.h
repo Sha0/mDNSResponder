c4fc8a4fc261ec3a7c847e66d8f0c36c9d76a961
herscher <herscher@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2005-09-12 06:13:32 +0000

    Bump version 1.0.2.1
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@3671 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSWindows/WinVersRes.h
+++ mDNSWindows/WinVersRes.h
@@ -23,6 +23,9 @@
     Change History (most recent first):
 
 $Log: WinVersRes.h,v $
+Revision 1.43  2005/09/12 06:13:32  herscher
+Bump version 1.0.2.1
+
 Revision 1.42  2005/07/22 19:41:44  ksekar
 Update version
 
@@ -157,10 +160,10 @@ First checked in.
 #define MASTER_PROD_NAME	"Bonjour"
 
 // Define the product version for mDNSResponder on Windows
-#define MASTER_PROD_VERS		1,0,1,2
-#define MASTER_PROD_VERS_STR	"1,0,1,2"
-#define MASTER_PROD_VERS_STR2	"1.0.1.2"
-#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.1.2"
+#define MASTER_PROD_VERS		1,0,2,1
+#define MASTER_PROD_VERS_STR	"1,0,2,1"
+#define MASTER_PROD_VERS_STR2	"1.0.2.1"
+#define MASTER_PROD_VERS_STR3 "Explorer Plugin 1.0.2.1"
 
 // Define the legal copyright
 #define MASTER_LEGAL_COPYRIGHT "Copyright (C) 2003-2005 Apple Computer, Inc."
