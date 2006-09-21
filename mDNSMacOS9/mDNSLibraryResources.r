e921f899aed8136cabe2b471a65881230e00dbeb
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2006-09-21 23:38:14 +0000

    mDNSResponder-112
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@3999 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSMacOS9/mDNSLibraryResources.r
+++ mDNSMacOS9/mDNSLibraryResources.r
@@ -17,6 +17,9 @@
     Change History (most recent first):
 
 $Log: mDNSLibraryResources.r,v $
+Revision 1.38  2006/09/21 23:38:13  cheshire
+mDNSResponder-112
+
 Revision 1.37  2006/08/14 23:24:29  cheshire
 Re-licensed mDNSResponder daemon source code under Apache License, Version 2.0
 
@@ -146,15 +149,15 @@ like Muse Research who want to be able to use mDNS/DNS-SD from GPL-licensed code
 
 resource 'vers' (1, purgeable)
 	{
-	0x01, 0x00, alpha, 111, verUS,
-	"1.0a111",
-	"Multicast DNS & DNS Service Discovery 1.0a111"
+	0x01, 0x00, alpha, 112, verUS,
+	"1.0a112",
+	"Multicast DNS & DNS Service Discovery 1.0a112"
 	};
 
 resource 'vers' (2, purgeable)
 	{
-   0x01, 0x00, alpha, 111, verUS,
-   "1.0a111",
+   0x01, 0x00, alpha, 112, verUS,
+   "1.0a112",
    "developer.apple.com/darwin/projects/bonjour/"
 	};
 
