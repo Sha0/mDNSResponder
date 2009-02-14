4f15649c24751947974b74b0d02ef7e8a6a1dfb0
cheshire <cheshire@214c2c4a-bf3b-4dcf-9390-e4dd3010487d>
2009-02-14 00:09:53 +0000

    Only log "Compiled without vproc_transaction support" when running on a system where we expect that to be available -- if running on a system that doesn't even have vproc_transaction, then it doesn't matter that the code was compiled without it.
    git-svn-id: http://svn.macosforge.org/repository/mdnsresponder/trunk@6204 214c2c4a-bf3b-4dcf-9390-e4dd3010487d

--- mDNSMacOSX/safe_vproc.c
+++ mDNSMacOSX/safe_vproc.c
@@ -17,6 +17,11 @@
     Change History (most recent first):
 
 $Log: safe_vproc.c,v $
+Revision 1.3  2009/02/14 00:09:53  cheshire
+Only log "Compiled without vproc_transaction support" when running on a system
+where we expect that to be available -- if running on a system that doesn't even
+have vproc_transaction, then it doesn't matter that the code was compiled without it.
+
 Revision 1.2  2009/02/09 21:16:17  mcguire
 <rdar://problem/5858533> Adopt vproc_transaction API in mDNSResponder
 additional cleanup: don't alloc memory since we currently only expect to have one transaction
@@ -33,33 +38,55 @@ Revision 1.1  2009/02/06 03:06:49  mcguire
 #include "mDNSDebug.h"
 
 #ifdef VPROC_HAS_TRANSACTIONS
+
 static vproc_transaction_t transaction = NULL;
-#endif
 
 void safe_vproc_transaction_begin(void)
 	{
-#ifdef VPROC_HAS_TRANSACTIONS
 	if (vproc_transaction_begin)
 		{
 		if (transaction) { LogMsg("safe_vproc_transaction_begin: Already have a transaction"); }
 		else transaction = vproc_transaction_begin(NULL);
 		}
-	else
-		LogMsg("vproc_transaction support unavailable");
-#else
-	LogMsg("Compiled without vproc_transaction support");
-#endif
 	}
 	
 void safe_vproc_transaction_end(void)
 	{
-#ifdef VPROC_HAS_TRANSACTIONS
 	if (vproc_transaction_end)
 		{
 		if (transaction) { vproc_transaction_end(NULL, transaction); transaction = NULL; }
 		else LogMsg("safe_vproc_transaction_end: No current transaction");
 		}
-	else
-		assert(transaction == NULL);
-#endif
 	}
+
+#else
+
+#include <stdio.h>
+#include <CoreFoundation/CFString.h>
+
+CF_EXPORT CFDictionaryRef _CFCopySystemVersionDictionary(void);
+CF_EXPORT const CFStringRef _kCFSystemVersionBuildVersionKey;
+#define OSXVers_10_6_SnowLeopard 10
+
+void safe_vproc_transaction_begin(void)
+	{
+	static int majorversion = 0;
+	if (!majorversion)
+		{
+		CFDictionaryRef vers = _CFCopySystemVersionDictionary();
+		if (vers)
+			{
+			char buildver[256];
+			CFStringRef cfbuildver = CFDictionaryGetValue(vers, _kCFSystemVersionBuildVersionKey);
+			if (cfbuildver && CFStringGetCString(cfbuildver, buildver, sizeof(buildver), kCFStringEncodingUTF8))
+				sscanf(buildver, "%d", &majorversion);
+			CFRelease(vers);
+			}
+		if (!majorversion || majorversion >= OSXVers_10_6_SnowLeopard)
+			LogMsg("Compiled without vproc_transaction support");
+		}
+	}
+
+void safe_vproc_transaction_end(void) { }
+
+#endif
